e9ea15a87f9c504537e69f44dcecb7f3d984491b
Jing Chen <jchen0506@gmail.com>
2021-01-12 23:30:24 -0600

    center of mass function and test added

--- molecool/functions.py
+++ molecool/functions.py
@@ -228,6 +228,40 @@ def calculate_molecular_mass(symbols):
     return mass
 
 
+def calculate_center_of_mass(symbols, coordinates):
+    """Calculate the center of mass of a molecule.
+
+    The center of mass is weighted by each atom's weight.
+
+    Parameters
+    ----------
+    symbols : list
+        A list of elements for the molecule
+    coordinates : np.ndarray
+        The coordinates of the molecule.
+
+    Returns
+    -------
+    center_of_mass: np.ndarray
+        The center of mass of the molecule.
+
+    Notes
+    -----
+    The center of mass is calculated with the formula
+
+    .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+
+    """
+    total_mass = calculate_molecular_mass(symbols)
+    mass_arr = np.zeros([len(symbols),1])
+
+    for i in range(len(symbols)):
+        mass_arr[i] = atomic_weights[symbols[i]]
+
+    center_of_mass = sum(coordinates * mass_arr) / total_mass
+    return center_of_mass
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
