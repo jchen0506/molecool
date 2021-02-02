11af346e3998fca4b10c0ca733c0e0e4e43b8d81
Jing Chen <jchen0506@gmail.com>
2021-02-02 12:57:47 -0600

    re-structure

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -1,7 +1,7 @@
 """
 Functions related to molecues
 """
-from .measure import calculate_distance;
+from .measure import calculate_distance
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
     if min_bond < 0:
